5940c45a343f7b9c01a1508810eb8f8c7f7d3f52
keisuke-hamazato <keihama24@gmail.com>
2026-01-21 15:42:54 +0800

    staff page

---
+++ resources/views/staffpage/edit-hotel.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.staff')
+
+@section('title', 'Edit Hotel')
+
+@section('content')
+    <!-- Content -->
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+
+                <h2 class="mb-4">Edit room</h2>
+
+                <div class="card shadow-sm">
+                    <div class="card-body p-4">
+
+                        <form action="#" method="post">
+                            @csrf
+                            @method('PATCH')
+
+                            <!-- Room type -->
+                            <div class="mb-3">
+                                <label class="form-label">
+                                    Room type <span class="text-danger">*</span>
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+
+                            <!-- Total rooms -->
+                            <div class="mb-4">
+                                <label class="form-label">
+                                    Total rooms <span class="text-danger">*</span>
+                                </label>
+                                <input type="number" class="form-control">
+                            </div>
+
+                            <!-- Buttons -->
+                            <button class="btn btn-primary w-100 mb-3">
+                                Edit
+                            </button>
+
+                            <a href="#" class="btn btn-outline-primary w-100">
+                                Cancel
+                            </a>
+                        </form>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+@endsection

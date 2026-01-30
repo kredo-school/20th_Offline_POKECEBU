940241a7b7905f5c9f93ce524f5d5c065b4aeb2e
Hikari-QQ <0321.rikahi@gmail.com>
2026-01-30 15:52:57 +0800

    update faq page

--- app/Models/FaqCategory.php
+++ app/Models/FaqCategory.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FaqCategory extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'soft_order'];
+
+    public function faqs() {
+        return $this->hasMany(Faq::class, 'faq_category_id');
+    }
 }

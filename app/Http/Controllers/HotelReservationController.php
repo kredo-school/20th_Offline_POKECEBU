15fa4fa80492af36d8858ddc11d0ad4de965afa7
tyaaaaeio <take0824bamboo@gmail.com>
2026-01-30 18:07:04 +0900

    ページ繋げる

--- app/Http/Controllers/HotelReservationController.php
+++ app/Http/Controllers/HotelReservationController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HotelReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('reservations.hotel');
+    }
 }
